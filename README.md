1cc6ea82a56b7115aadf856c83073a76d4dcfabd
Mohamed Shafeeque <mshafeequemsn@gmail.com>
2020-11-11 10:12:41 +0530

    added Math function

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# myjavascriptapp
+# My JavaScript app - Shopping cart
+A small JavaScript application created to learn the usages of Math
--- js/main.js
+++ js/main.js
@@ -1,17 +1,35 @@
-// var addProduct = function(){
-//     var button = document.getElementById("btnAddProduct");
-//     var productName = document.getElementById("productName");
-//     var productPrice = document.getElementById("productPrice");
-//     var productDetails = document.getElementById("productDetails");
-//     productArray = [];
-//     button.addEventListener('click', function(){
-//         productName = productName.value;
-//         productPrice = productPrice.value;
-//         productDetails = productDetails.value;
-//     });
-// }
-// addProduct();
+function roll(min, max, floatRange) {
+    let r = Math.random() * (max - min) + min;
+    return floatRange ? r : Math.floor(r)
 
-var xhr = new XMLHttpRequest();
-xhr.open("get","database.json");
-console.log(xhr.send());
+}
+
+let possibleProducts = ["🍇", "🍈", "🍉", "🍊", "🍋", "🍌", "🍍", "🥭", "🍎", "🍏", "🍐", "🍑", "🍒", "🍓", "🥝", "🍅", "🥥", "🥑", "🍆", "🥔", "🥕", "🌽", "🌶", "🥒", "🥬", "🥦"]
+
+let products = [...Array(5)].map((_, i) => {
+    return {
+        index: i,
+        name: possibleProducts[roll(0, possibleProducts.length)],
+        price: roll(1, 10, 1).toFixed(2),
+        count: roll(1, 5)
+    }
+})
+
+console.log(products)
+
+let productsElement = document.getElementById('products');
+let cartHtml = '';
+products.forEach((product) => {
+    cartHtml += `<div class="product">
+        <div>${product.name}</div>
+        <div>x${product.count}</div>
+        <div>$${product.price}</div>
+    </div>`
+});
+
+productsElement.innerHTML = cartHtml;
+
+
+
+
+
